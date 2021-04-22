22bfe00b77efd12a0f6cf9c5e273c8c76900dcd1
Horace Spencer <horacefagsao@gmail.com>
2021-04-21 23:37:25 -0400

    Implemented Redux for state and added cart

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+
+import rootReducer from './root-reducer'
+
+const middlewares = [logger]
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+export default store
+
+//+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
